24b9294e3df05ebef678b2400054ee43123195ec
Guss <infojobgus@gmail.com>
2022-08-08 21:48:58 +0000

    javascript added

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const navbarlinks = document.getElementsByClassName('navbar-links')[0]
 togglebutton.addEventListener('click', () => {
     navbarlinks.classList.toggle('active')
 })
+
+
+let p = document.getElementById('adr'); 
+    p.onmouseover= ev => {
+    p.style.backgroundColor = 'grey'
+    }
+    {
+    p.onmouseleave = ev => {
+        p.style.backgroundColor = 'white'
+    } 
+    }
+    
